14e49da208b6d841d61384452c06d0225acba5b6
idolice <349031587@qq.com>
2016-05-24 09:47:47 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,31 @@
+//TODO: Please write code in this file.
+function printInventory(inputs) {
+
+    var products=[];
+    inputs.forEach(function(e){
+      if(products[e.barcode])
+       products[e.barcode].num++;
+       else{
+         products[e.barcode]=new Object();
+         products[e.barcode].num=1;
+         products[e.barcode].name=e.name;
+         products[e.barcode].barcode=e.barcode;
+         products[e.barcode].price=e.price;
+         products[e.barcode].unit=e.unit;
+       }
+    });
+
+    var total=0;
+
+    var text="***<没钱赚商店>购物清单***\n";
+    for(var i=0;i<products.length;i++)
+    {
+      text=text+"名称："+products[i].name+"，数量："+products[i].num+products[i].unit+"，单价："+products[i].price.toFixed(2)+"（元），小计："+products[i].num.*products[i].price+"（元）"+"\n";
+      total+=products[i].num*products[i].price;
+
+    };
+    text=text+"总计："+total.toFixed(2)+"（元）";
+    console.log(text);
+
+
+}
